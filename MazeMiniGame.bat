34d36f1f3df417c00aed9d8a5c0302093af4e2f3
ngngka1 <ngwewe2015@gmail.com>
2024-01-29 21:33:56 +0800

    added packages installation etc. in bat file

--- MazeMiniGame.bat
+++ MazeMiniGame.bat
@@ -1,6 +1,27 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+if not "%OS%" == "Windows_NT" (
+    echo This game is only supported on window for now!
+    timeout /t 5 > nul
+    exit
+)
+
+dir /b /ad | findstr /i /x /c:"venv" > nul
+if !errorlevel! neq 0 (
+    echo Installing virtual environment...
+    python -m venv venv
+)
+
 call venv\Scripts\activate.bat
 
-python main.py
+for /F "tokens=*" %%A in (requirements.txt) do (
+    pip freeze | findstr /i /x /c:"%%A"
+    if !errorlevel! neq 0 (
+        echo installing %%A...
+        pip install %%A
+    )
+)
 
+python main.py
 call venv\Scripts\dectivate.bat
